5fbe917c0755cfbc335b933ad1a7dbec3629ad26
arcursino <arcursino@gmail.com>
2019-07-04 14:54:38 -0300

    adiciona desafio 1001

---
+++ maratona/1001_ucoder.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*int main() {
+	int a, b, c;
+
+    scanf("%d %d", &a, &b);
+
+    c = a + b;
+
+    printf("%d\n", c);
+
+    return 0;
+}*/
+
+int fat(int n);
+
+int main() {
+    int n;
+
+    n = 1;
+
+    while (scanf("%d", &n) >= 0 && n <= 12) {
+        printf("%d\n", fat(n));
+        break;
+    }
+
+    return 0;
+}
+
+int fat(int n) {
+    if (n == 0) {
+        return n + 1;
+
+    } else {
+        return n * fat(n-1);
+    }
+}
