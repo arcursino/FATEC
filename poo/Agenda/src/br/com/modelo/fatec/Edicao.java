10dce0a9533a5c0065731d5bfb967095167e11ad
ariana <arcursino@gmail.com>
2019-09-19 08:32:08 -0300

    resolucao de listas

--- poo/Agenda/src/br/com/modelo/fatec/Edicao.java
+++
@@ -1,5 +0,0 @@
-package br.com.modelo.fatec;
-
-public class Edicao {
-
-}
--- so/ex3/thread.c
+++ so/ex3/thread.c
@@ -1,26 +1,51 @@
-#include <pthread.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
-#define NUMBER_OF_THREADS 10
+/*Rotina que será executada*/
+void * routine(void *arg);
 
-void *print_hello_world (void *tid) {
-    printf("Hello World. Greetings from thread %d\n", tid);
-    pthread_exit(NULL);
+int main (int argc, char *argv[])
+{
+	pthread_t thread_id;
+	void * thread_res;
+	int rstatus;
+	
+	/*tenta iniciar o thread, indicando a função 'routine' e passando como argumento a string 
+    "Minha primeira Thread!"*/
+	rstatus = pthread_create (&thread_id, NULL, routine, (void*)("Minha primeira Thread!"));
+
+	/*verificar se ocorreu algum erro na chamada de pthread_create*/
+	if(rstatus != 0)
+	{
+		printf ("Erro ao criar o thread.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf ("Thread criado com sucesso!\n");
+
+	/*aguarda finalização do thread identificado por thread_id. O retorno é passado pelo ponteiro 
+    thread_res*/
+	rstatus = pthread_join (thread_id, &thread_res);
+
+	/*verificar se ocorreu algum erro na chamada de pthread_join*/
+	if(rstatus != 0)
+	{
+		printf ("Erro ao aguardar finalização do thread.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*exibe o valor de retorno da função 'routine'*/
+	printf ("Thread finalizado! Retorno = %s\n", (char *)thread_res);
+
+	return 0;
 }
 
-int main (int argc, char *argv[]) {
-    pthread_t threads[NUMBER_OF_THREADS];
-    int status, i;
+void * routine(void *arg)
+{
+	/*exibe o argumento recebido*/
+	printf("Argumento: %s\n", (char *)arg);
 
-    for(i = 0; i < NUMBER_OF_THREADS; i++) {
-        printf("Main here. Creating thread%d\n", i);
-        status = pthread_create(&threads[i], NULL, print_hello_world, (void *)i);
-
-        if(status != 0) {
-            printf("Oops... pthread_create returned error code %d\n", status);
-            exit(-1);
-        }
-    }
-    exit(NULL);
+	/*finaliza a função retornando o argumento que foi recebido*/
+	pthread_exit(arg);
 }
